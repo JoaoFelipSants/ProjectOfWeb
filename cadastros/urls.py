d1bac0c38e25ff09be99d1a1c8a2406c29cd4867
Iago Gonçalves de Meira <iagomeiraifpr@gmail.com>
2022-01-31 09:46:05 -0300

    Servidor consertado

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,19 +1,17 @@
-from importlib.resources import path
-from os import name
-from django.db import models
+from django.urls import path
 from .views import PerfilCreate, PerfilUpdate, CategoriaCreate, CategoriaUpdate, NoticiaCreate, NoticiaUpdate
 
 urlpatterns = [
     #todas as rotas vao ser assim
     #path('inicio/', PaginaInicial.as_view(),name='index'),
 
-   path('cadastrar/perfil/', PerfilCreate.as_view(), name='cadastrar_perfil'),
-   path('editar/perfil/<int:pk/>', PerfilUpdate.as_view(), name='editar_perfil'),
+    path('cadastrar/perfil/', PerfilCreate.as_view(), name='cadastrar_perfil'),
+    path('editar/perfil/<int:pk>/', PerfilUpdate.as_view(), name='editar_perfil'),
 
-   path('cadastrar/categoria/', CategoriaCreate.as_view(), name='cadastrar_categoria'),
-   path('editar/categoria/<int:pk/>', CategoriaUpdate.as_view(), name='editar_categoria'),
+    path('cadastrar/categoria/', CategoriaCreate.as_view(), name='cadastrar_categoria'),
+    path('editar/categoria/<int:pk>/', CategoriaUpdate.as_view(), name='editar_categoria'),
 
-   path('cadastrar/noticia/', NoticiaCreate.as_view(), name='cadastrar_noticia'),
-   path('editar/noticia/<int:pk/>', NoticiaUpdate.as_view(), name='editar_noticia'),
+    path('cadastrar/noticia/', NoticiaCreate.as_view(), name='cadastrar_noticia'),
+    path('editar/noticia/<int:pk>/', NoticiaUpdate.as_view(), name='editar_noticia'),
 
 ]
--- paginas/urls.py
+++ paginas/urls.py
@@ -1,12 +1,9 @@
-from os import name
 from django.urls import path
 from .views import PaginaInicial, Sobre
 
 urlpatterns = [
     #todas as rotas vao ser assim
     path('inicio/', PaginaInicial.as_view(),name='index'),
-    path('', PaginaInicial.as_view(), name='index-2'),
-
-    path('sobre/', Sobre.as_view(), name='sobre'),
-
+    path('', PaginaInicial.as_view(), name='index'),
+    path('sobre/', Sobre.as_view(), name='index'),
 ]
