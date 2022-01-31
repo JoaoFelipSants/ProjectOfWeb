33a94b644c87273c274f7e9ace4e3db99056ff95
unknown <joao.pessoal.ifpr@gmail.com>
2022-01-31 18:05:00 -0300

    atualizado com a aula doze

--- paginas/urls.py
+++ paginas/urls.py
@@ -4,6 +4,7 @@ from .views import PaginaInicial, Sobre
 urlpatterns = [
     #todas as rotas vao ser assim
     path('inicio/', PaginaInicial.as_view(),name='index'),
-    path('', PaginaInicial.as_view(), name='index'),
-    path('sobre/', Sobre.as_view(), name='index'),
+    path('', PaginaInicial.as_view(), name='index-2'),
+    
+    path('sobre/', Sobre.as_view(), name='sobre'),
 ]
